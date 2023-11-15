a8f3a6f886030b4132ce6610c261190852b8efb5
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2023-11-15 22:35:58 +0100

    somehow works

--- scrapper.js
+++ scrapper.js
@@ -4,23 +4,23 @@ async function getPosts() {
     console.log("feed length: " + feed.children.length)
 	let result = [];
 
-    for (let i = 1; i < feed.children.length && i < 2; i++) {
+    for (let i = 1; i < feed.children.length && i < 10; i++) {
         console.log("post " + i)
-        post = feed.children[i]
+        let post = feed.children[i]
         post.scrollIntoView()
-        await sleep(500)
+		await waitUntil(() => post.children.length)
 
         //text = document.evaluate(".//div[@data-ad-preview='message']", post, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent
-        authorNode = document.evaluate(".//h3", post, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+        let authorNode = document.evaluate(".//h3", post, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
         if (authorNode == null) {
             continue
         }
-        author = authorNode.textContent
+        let author = authorNode.textContent
 
-        links = document.evaluate(".//a", post, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
-        postLinkNode = null
+        let links = document.evaluate(".//a", post, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
 
-        for (let j = 0; j < links.snapshotLength; j++) {
+		const maxLinks = 4;
+        for (let j = 0; j < links.snapshotLength && j < maxLinks; j++) {
             link = links.snapshotItem(j)
 
             let event = new FocusEvent('focusin', {
@@ -32,31 +32,60 @@ async function getPosts() {
             link.dispatchEvent(event);
         }
 
-        for (let j = 0; j < links.snapshotLength; j++) {
-            link = links.snapshotItem(j)
-            // if link is of form ".../groups/.../posts/..." then it is the link to the post
-            if (link.attributes['href'].nodeValue.includes("/posts/")) {
-                postLinkNode = link
-                break
-            }
-        }
+		let postLinkNode = await waitUntil(() => {
+			let value = null;
+			for (let j = 0; j < links.snapshotLength && j < maxLinks; j++) {
+				link = links.snapshotItem(j);
+				if (link.attributes['href'].nodeValue.includes("/posts/")) {
+					value = link;
+					break;
+				}
+			}
+			return value;
+		})
 
         if (!postLinkNode) {
             console.log("no post link found")
             break
         }
 
-        date = getFirstText(postLinkNode)
-        postLink = postLinkNode.attributes['href'].nodeValue
+        let date = getFirstText(postLinkNode)
+        let postLink = postLinkNode.attributes['href'].nodeValue
+
+		let textNode = document.evaluate(".//div[text() != '']", post, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+		while (textNode.nodeName.toLowerCase() != "span") {
+			textNode = textNode.parentElement;
+		}
+		let text = textNode.textContent;
 
         // Log date, author, link
         console.log(date + " " + author + " " + postLink)
 		result.push({
 			date: date,
 			author: author,
-			link: postLink
+			link: postLink,
+			text: text
 		})
     }
+
+	return result;
+}
+
+async function waitUntil(func) {
+	console.log("waitUntil")
+	let timeout = 10000;
+	let begin = new Date().getTime();
+	console.log("waitUntil, begin: " + begin)
+	let result = func();
+
+	console.log("waitUntil, result: " + result)
+	while (!result && new Date().getTime() - begin < timeout) {
+		let sleepTime = Math.random() * 1000;
+		console.log("waitUntil, sleep: " + sleepTime)
+		await sleep(sleepTime);
+		result = func();
+		console.log("waitUntil, result: " + result)
+	}
 
 	return result;
 }
@@ -98,7 +127,11 @@ function sleep(ms) {
 
 (async () => {
 	var result = new Object();
-	result.debug = "I'm alive!";
-	result.output = await getPosts();
+	try {
+		result.debug = "I'm alive!";
+		result.output = await getPosts();
+	} catch (e) {
+		result.debug = e;
+	}
 	return result;
 })();
